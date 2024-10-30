02dc90eb397e002db76219ee5bcf7d6c6db5b594
SpaceFabrics <76684574+SpaceFabrics@users.noreply.github.com>
2024-10-29 21:52:28 -0400

    Create TemperatureConversion.py

---
+++ TemperatureConversion.py
@@ -0,0 +1,24 @@
+def getTemps():
+    temp = float(input("input temperature value"))
+    unit = input("C or F?")
+    return unit, temp
+
+def tempSelect(unit, temp):
+    if unit == "C":
+        result = calc(temp)
+        print(result)
+    else:
+        result = calf(temp)
+        print(result)
+
+def calc(temp):
+    answr = float((temp * (9/5)) + 32)
+    return answr
+
+def calf(temp):
+    answr = float((temp - 32) * (5/9))
+    return answr
+
+unit, temp = getTemps()
+tempSelect(unit, temp)
+

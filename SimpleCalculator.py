045432573d585232fd645efcdb99bfaedcbb5810
SpaceFabrics <76684574+SpaceFabrics@users.noreply.github.com>
2024-10-29 21:39:16 -0400

    Create SimpleCalculator.py

---
+++ SimpleCalculator.py
@@ -0,0 +1,12 @@
+def numbers():
+    n1 = int(input("What is the first number?"))
+    n2 = int(input("What is the second number?"))
+    return n1, n2
+
+def addition(num1, num2):
+    a1 = int(num1 + num2)1
+    print("the result is:", a1)
+
+num1, num2 = numbers()
+addition(num1, num2)
+

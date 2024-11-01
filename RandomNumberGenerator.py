f14880084385b082455cc2ab22d6dcd8590c53ea
SpaceFabrics <76684574+SpaceFabrics@users.noreply.github.com>
2024-11-01 02:11:39 -0400

    Update RandomNumberGenerator.py

--- RandomNumberGenerator.py
+++ RandomNumberGenerator.py
@@ -1,17 +1,28 @@
+import random
+
+
+version = 0.01
+
 i = 0
+r = random.randint(0,10)
 
 def lives():
     amt = input("How many lives do you want?")
     return amt
 
 def guess():
-    guess = int(input("What is your guess?"))
+    guess = 1int(input("What is your guess 0-10?"))
     return guess
 
-def check(g):
-    num = 10
+def check(r, g, i):
+    num = r
+    live = i
     if g == num:
         print("You won!")
+    elif g == "":
+        print("seriously...?")
+        i -= 1
+        return i
     else:
         print("Wrong!")
 
@@ -19,5 +30,5 @@ l = int(lives())
 
 while i < l:
     g = guess()
-    check(g)
+    check(g, r, i)
     i += 1
